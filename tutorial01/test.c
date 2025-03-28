5f80b9faab7efd7de533cd48b003d7c96b6cf2c1
pengzj23laptop <965923386@qq.com>
2025-03-28 22:07:14 +0800

    This is my first git commit!

--- tutorial01/test.c
+++ tutorial01/test.c
@@ -66,6 +66,6 @@ static void test_parse() {
 
 int main() {
     test_parse();
-    printf("%d/%d (%3.2f%%) passed\n", test_pass, test_count, test_pass * 100.0 / test_count);
+    printf("%d/%d (%3.2f%%) passed\n This is my first try 2025年3月28日\n", test_pass, test_count, test_pass * 100.0 / test_count);
     return main_ret;
 }
