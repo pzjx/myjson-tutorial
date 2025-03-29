d657136fd95cc15d1856a1f5a6524258676ffaab
pengzj23laptop <965923386@qq.com>
2025-03-29 20:14:01 +0800

    successfully passed test01

--- tutorial01/leptjson.c
+++ tutorial01/leptjson.c
@@ -2,7 +2,7 @@
 #include <assert.h>  /* assert() */
 #include <stdlib.h>  /* NULL */
 
-#define EXPECT(c, ch)       do { assert(*c->json == (ch)); c->json++; } while(0)
+#define EXPECT(c, ch) do { assert(*c->json == (ch)); c->json++; } while(0)
 
 typedef struct {
     const char* json;
@@ -24,10 +24,30 @@ static int lept_parse_null(lept_context* c, lept_value* v) {
     return LEPT_PARSE_OK;
 }
 
+static int lept_parse_true(lept_context* c, lept_value* v) {
+    EXPECT(c, 't');
+    if (c->json[0] != 'r' || c->json[1] != 'u' || c->json[2] != 'e')
+        return LEPT_PARSE_INVALID_VALUE;
+    c->json += 3;
+    v->type = LEPT_TRUE;
+    return LEPT_PARSE_OK;
+}
+
+static int lept_parse_false(lept_context* c, lept_value* v) {
+    EXPECT(c, 'f');
+    if (c->json[0] != 'a' || c->json[1] != 'l' || c->json[2] != 's' || c->json[3] != 'e')
+        return LEPT_PARSE_INVALID_VALUE;
+    c->json += 4;
+    v->type = LEPT_FALSE;
+    return LEPT_PARSE_OK;
+}
+
 static int lept_parse_value(lept_context* c, lept_value* v) {
     switch (*c->json) {
         case 'n':  return lept_parse_null(c, v);
         case '\0': return LEPT_PARSE_EXPECT_VALUE;
+        case 't':  return lept_parse_true(c, v);
+        case 'f':  return lept_parse_false(c, v);
         default:   return LEPT_PARSE_INVALID_VALUE;
     }
 }
@@ -36,9 +56,13 @@ int lept_parse(lept_value* v, const char* json) {
     lept_context c;
     assert(v != NULL);
     c.json = json;
-    v->type = LEPT_NULL;
+    v->type = LEPT_NULL;        
     lept_parse_whitespace(&c);
-    return lept_parse_value(&c, v);
+    int ret = lept_parse_value(&c, v);
+    lept_parse_whitespace(&c);
+    if (c.json[0] != '\0' && ret == LEPT_PARSE_OK)  // Check if there are extra characters after parsing
+        return LEPT_PARSE_ROOT_NOT_SINGULAR;
+    return ret;
 }
 
 lept_type lept_get_type(const lept_value* v) {
